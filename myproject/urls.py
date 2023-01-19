85b2e16813db4834584c48f2bafc25e0bceb185d
umer-farooq-dev <ufkeyfans786@gmail.com>
2023-01-19 23:15:59 +0500

    Urls add

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls'))
+    path('admin/', admin.site.urls),
+    path('chat/', include('chat.urls'))
 ]
