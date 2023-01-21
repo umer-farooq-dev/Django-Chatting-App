5393f25dcc1524addc741e782b14e4abde787f12
umer-farooq-dev <ufkeyfans786@gmail.com>
2023-01-21 22:32:04 +0500

    Seetings Added

--- myproject/settings.py
+++ myproject/settings.py
@@ -149,3 +149,12 @@ CHANNEL_LAYERS = {
         # }
     }
 }
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        # 'CONFIG': {
+        #     'hosts': [('127.0.0.1', 6379)],
+        # }
+    }
+}
