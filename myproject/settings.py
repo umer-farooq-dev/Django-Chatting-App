66fe0cb2a0856e08194b4db3199b40e809fd8e5d
umer-farooq-dev <ufkeyfans786@gmail.com>
2023-01-25 23:20:25 +0500

    Seetings Added

--- myproject/settings.py
+++ myproject/settings.py
@@ -161,3 +161,15 @@ CHANNEL_LAYERS = {
     }
 }
 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        # 'CONFIG': {
+        #     'hosts': [('127.0.0.1', 6379)],
+        # }
+    }
+}
+
